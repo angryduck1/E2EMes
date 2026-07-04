f785b5caf2f4529a5f871262dc0d4e270dc8aa9f
makss4315 <makss4315@gmail.com>
2026-07-04 21:28:20 +0200

    -fix qss file linking -fix qss styles -add some styles for buttons -some change ui file structure

--- UI/mainwindow.cpp
+++ UI/mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 #include <iostream>
 #include <QFile>
+#include <QMessageBox>
 #include <thread>
 #include <QStackedWidget>
 
@@ -10,8 +11,21 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    QFile fileQss(":/resources.qss");    //icon inside LoginUserName
+    if (fileQss.open(QFile::ReadOnly))
+    {
+        QString style = QLatin1String(fileQss.readAll());
+        setStyleSheet(style);
+        fileQss.close();
+        
 
-    setFixedSize(1280,720);
+    } else
+    {
+        QMessageBox::critical(this, "Error", "resources.qss not found!");
+        return;
+    }
+
+     setFixedSize(1280,720);
     ui->setupUi(this);
     ui->stackedWidget->setCurrentIndex(0);
     connect(this, &MainWindow::loginTrySignal, this, &MainWindow::on_loginTrySignal);
@@ -21,10 +35,7 @@ MainWindow::MainWindow(QWidget *parent) :
     QIcon lockIcon(":/icons/lock.svg");
    ui->lockLabel->setPixmap(lockIcon.pixmap(60,60));
     ui->lockLabel->setAlignment(Qt::AlignCenter);
-    ui->lockLabel->setStyleSheet(
-    "background-color: #6C3FD6;"
-    "border-radius: 40px;"
-);
+
 
     QIcon shieldIcon(":/icons/shield.svg");
     ui->shieldLabel->setPixmap(shieldIcon.pixmap(20,20));
@@ -32,13 +43,8 @@ MainWindow::MainWindow(QWidget *parent) :
 
 
     //page 1
-    QFile fileQss(":/styles/resources.qss");    //icon inside LoginUserName
-    if ( fileQss.open(QFile::ReadOnly))
-    {
-        setStyleSheet(QLatin1String(fileQss.readAll()));
 
-        fileQss.close();
-    }
+
     QAction *userLoginNameIcon = new QAction(this);
     userLoginNameIcon->setIcon(QIcon(":/icons/user.svg"));
     ui->userLoginName->addAction(userLoginNameIcon, QLineEdit::LeadingPosition);
@@ -46,6 +52,9 @@ MainWindow::MainWindow(QWidget *parent) :
     QAction *userLoginPasswordIcon = new QAction(this);
     userLoginPasswordIcon->setIcon(QIcon(":/icons/key.svg"));
     ui->userLoginPassword->addAction(userLoginPasswordIcon, QLineEdit::LeadingPosition);
+
+
+
 }
 
 MainWindow::~MainWindow()
